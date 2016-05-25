d28ba8ca3c20bdb3aeeab6600f845eeb12d64679
Paul Mauviel <ozzadar@ozzadar.com>
2016-05-25 16:54:46 +0700

    Changes to make it google compatible

--- docker-flow-proxy-demo-environments.sh
+++ docker-flow-proxy-demo-environments.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-docker-machine create -d virtualbox proxy
+docker-machine create -d google \
+    --google-project silken-zenith-132017 \
+    proxy
 
 export DOCKER_IP=$(docker-machine ip proxy)
-
 export CONSUL_IP=$(docker-machine ip proxy)
+export CONSUL_INTERNAL=$(gcloud compute instances describe proxy --format=text     | grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g')
+export GOOGLE_PROJECT_NAME=silken-zenith-132017
 
 eval "$(docker-machine env proxy)"
 
@@ -16,25 +19,28 @@ sleep 2
 
 docker-compose up -d proxy
 
-docker-machine create -d virtualbox \
+docker-machine create -d google \
+    --google-project $GOOGLE_PROJECT_NAME \
     --swarm --swarm-master \
     --swarm-discovery="consul://$CONSUL_IP:8500" \
     --engine-opt="cluster-store=consul://$CONSUL_IP:8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     swarm-master
 
-docker-machine create -d virtualbox \
+docker-machine create -d google \
+    --google-project $GOOGLE_PROJECT_NAME \
     --swarm \
     --swarm-discovery="consul://$CONSUL_IP:8500" \
     --engine-opt="cluster-store=consul://$CONSUL_IP:8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     swarm-node-1
 
-docker-machine create -d virtualbox \
+docker-machine create -d google \
+    --google-project $GOOGLE_PROJECT_NAME \
     --swarm \
     --swarm-discovery="consul://$CONSUL_IP:8500" \
     --engine-opt="cluster-store=consul://$CONSUL_IP:8500" \
-    --engine-opt="cluster-advertise=eth1:2376" \
+    --engine-opt="cluster-advertise=eth0:2376" \
     swarm-node-2
 
 eval "$(docker-machine env swarm-master)"
